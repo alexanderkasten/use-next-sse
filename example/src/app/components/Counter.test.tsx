ab8a8138cba21e609d58cb5d9123345b023494a4
Alexander Kasten <alexander.kasten@5minds.de>
2024-12-26 13:30:07 +0100

    Refactor Counter.test.tsx to import '@testing-library/jest-dom' directly

--- example/src/app/components/Counter.test.tsx
+++ example/src/app/components/Counter.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Counter from './Counter';
 import { useSSE } from 'use-next-sse';
+
+import Counter from './Counter';
 
 jest.mock('use-next-sse');
 
